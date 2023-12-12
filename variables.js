a9913e784cdc4094834cb2517e7cd4b3a0b9af32
Awesom12 <sujana.kotike@gmail.com>
2023-12-11 22:52:34 -0700

    Adding in Variables.js

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Comments here
+
+//Numbers
+
+let x = 5;
+console.log(x);
+
+let y = 10.5;
+console.log(y);
+
+//Strings
+
+let z = "Hello"
+console.log(z);
+
+let a = "A string can be a whole sentence";
+console.log(a);
+
+//Boolean
+
+let b = true;
+console.log(b);
